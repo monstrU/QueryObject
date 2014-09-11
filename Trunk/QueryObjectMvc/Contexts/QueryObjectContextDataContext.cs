f48394ccdc1756a7b90b391296cd459505973afa
Alexander Zaytsev <hazzik@gmail.com>
2014-09-12 09:15:53 +1200

    IStorage implementation improvement
    * move custom code to a separate file
    * fix IStorage.Authors implementation

---
+++ Trunk/QueryObjectMvc/Contexts/QueryObjectContextDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using QueryObjectMvc.Interfaces;
+
+namespace QueryObjectMvc.Contexts
+{
+    public partial class QueryObjectContextDataContext : IStorage
+    {
+        IQueryable<Author> IStorage.Authors
+        {
+            get { return Authors; }
+        }
+    }
+}
